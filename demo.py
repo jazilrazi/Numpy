cbbb1a5093c93cc895736675ec11cc44cfba74ff
jazilrazi <jazilrazi03@gmail.com>
2026-08-31 23:57:44 +0530

    Basic math operations

---
+++ demo.py
@@ -0,0 +1,49 @@
+import numpy as np
+# a = np.array([1,2,3],dtype=complex)
+# print(a)
+
+# dt = np.dtype(np.int32)
+# print(dt)
+
+# dt = np.dtype('i4')
+# print(dt)
+
+# dt = np.dtype([('age',np.int8)])
+# print(dt)
+
+# dt = np.dtype([('age',np.int8)])
+# a = np.array([(10),(20),(30)],dtype=dt)
+# print(a['age'])
+
+
+# student =np.dtype([('name','S20'),('age','i1'),('marks','f4')])
+# print(student)
+
+# student =np.dtype([('name','S20'),('age','i1'),('marks','f4')])
+# a = np.array([('jazil',22,100),('razi',21,99)],dtype=student)
+# print(a)
+
+
+
+# a = np.array([[1,2,3], [4,5,6]])
+# print(a)
+# print(a.shape)
+
+# a=np.array([[1,2,3],[4,5,6]])
+# print(a)
+# print(a.ndim)
+
+# x = np.array([1,2,3,4,5],dtype = np.int8)
+# print(x)
+# print(x.itemsize)
+
+# x = np.array([1,2,3,4,5], dtype = np.float32)
+# print(x)
+# print(x.itemsize)
+
+# x = np.zeros(5)
+# print(x)
+
+# x = np.zeros([2,2], dtype ='int8')
+# print(x)
+
